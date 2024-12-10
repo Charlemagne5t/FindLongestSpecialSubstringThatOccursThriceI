8785e1d4e85faaa9377cd3f6f20089efdc09d7b8
alek <wait82985@gmail.com>
2024-12-10 07:22:33 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,34 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     int res = -1;
-    public int maximumLength(String s) {
-        Map<String, Integer> map = new HashMap<>();
-        boolean[] starts = new boolean[s.length()];
-        starts[0] = true; 
-        dfs(s, 1, s.charAt(0) + "", map, starts);
-       
-        return res;
-    }
 
-    void dfs(String s, int i, String cur,  Map<String, Integer> map, boolean[] starts ) {
-        
-        int c = map.getOrDefault(cur, 0);
-        c++;
-        if(c == 3 && cur.length() != 0) {
-            res = Math.max(res, cur.length()); 
+    public int maximumLength(String s) {
+        Map<String, Integer> map = new gHashMap<>();
+        int count = 1;
+        int res = -1;
+        for (int i = 1; i <= s.length(); i++) {
+            if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                int c = map.getOrDefault("" +s.charAt(i - 1) + count, 0);
+                c++;
+                if (c > 2) {
+                    res = Math.max(count, res);
+                }
+                map.put("" + s.charAt(i - 1) + count, c);
+                if (count > 1) {
+                    c = map.getOrDefault("" +s.charAt(i - 1) + (count - 1), 0);
+                    c += 2;
+                    if (c > 2) {
+                        res = Math.max(count - 1, res);
+                    }
+                    map.put("" +s.charAt(i - 1) + (count - 1), c);
+                }
+                if (count > 2) {
+                    c = map.getOrDefault("" +s.charAt(i - 1) + (count - 2), 0);
+                    c += 3;
+                    if (c > 2) {
+                        res = Math.max(count - 2, res);
+                    }
+                    map.put("" +s.charAt(i - 1) + (count - 2), c);
+                }
+                count = 1;
+            }
+
         }
-        map.put(cur, c);
-        if(i == s.length()) {
-            return;
-        }
-        if(i < s.length()&& cur.charAt(cur.length() - 1) == s.charAt(i)) {
-            dfs(s, i + 1, cur + s.charAt(i), map, starts);;
-        }
-        
-        if(!starts[i]){
-            starts[i] = true;
-            dfs(s, i + 1, "" + s.charAt(i), map, starts);
-            
-        }
-        
+
+        return res;
+
     }
 }
